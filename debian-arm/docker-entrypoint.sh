92375a9046a4fc2dcca9d88963ba6f5cd7f97c9c
Ezra Buehler <ezra@easyb.ch>
2025-06-22 01:22:34 +0200

    debian-arm: add sudo support

--- debian-arm/docker-entrypoint.sh
+++ debian-arm/docker-entrypoint.sh
@@ -5,6 +5,8 @@ GROUP_ID=${GID:-1000}
 
 getent group user >/dev/null || groupadd -g "$GROUP_ID" -o user
 id user >/dev/null 2>&1 || \
-    useradd -s /bin/bash -u "$USER_ID" -g user -o -d /home/user user
+    useradd -s /bin/bash -u "$USER_ID" -g user -G sudo -o -d /home/user user && \
+    echo "%sudo	ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/user && \
+    chmod 440 /etc/sudoers.d/user
 
 exec gosu user "$@"
